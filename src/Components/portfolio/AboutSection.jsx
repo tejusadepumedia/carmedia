bca5ba578a1ed565d182cd97a25c5945f1ea62bd
tejus-adepu-codes <TejAdepu1@outlook.com>
2026-01-28 11:03:09 -0500

    fix img path. removed another dupe

--- src/Components/portfolio/AboutSection.jsx
+++ src/Components/portfolio/AboutSection.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import btsimage from "/src/photos_compressed/lexus/DSC05840.webp";
+import btsimage from "/src/photos_compressed/3isVyomSnow/DSC07598.webp";
 
 
 export default function AboutSection() {
